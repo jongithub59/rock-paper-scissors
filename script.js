e0b586de67a36312fd0810f9ac21ea71d5f7aa37
jongithub59 <jdelrealj01@gmail.com>
2023-09-05 18:56:17 -0500

    added rounds to game and score keeping changed the structure to be more clean and functional and used a while loop to play the game until someone wins 5 times. added variables to track round wins and if statement to decid the winner

--- script.js
+++ script.js
@@ -17,36 +17,58 @@ function getComputerChoice() { // randomly generates a numeber 1-3 and uses a sw
 	
 }
 
-function playRound(playerChoice, computerChoice) {
+// Take the player's selection and adds a point for the winner of the round
+function playRound() {
+	const computerChoice = getComputerChoice();
+	let playerChoice = prompt('Choose rock, paper, or scissors.');
+	playerChoice = playerChoice.toLowerCase();
+
 	if (playerChoice == computerChoice) {
 		return('Tie!');
 
 	} else if(playerChoice == 'rock' && computerChoice == 'paper') {
+		computerWins = computerWins + 1;
 		return('Computer chose Paper! You Lose! Paper beats Rock!');
+		
 
 	} else if (playerChoice == 'rock' && computerChoice == 'scissors') {
+		playerWins = playerWins + 1;
 		return('Computer chose Scissors! You Win! Rock beats Scissors!');
 
 	} else if (playerChoice == 'paper' && computerChoice == 'rock') {
+		playerWins = playerWins + 1;
 		return('Computer chose Rock! You Win! Paper beats Rock!');
 
 	} else if (playerChoice == 'paper' && computerChoice == 'scissors') {
+		computerWins = computerWins + 1;
 		return('Computer chose Scissors! You Lose! Scissors beats Paper!');
 
 	} else if (playerChoice == 'scissors' && computerChoice == 'rock') {
+		computerWins = computerWins + 1;
 		return('Computer chose Rock! You Lose! Rock beats Scissors!');
 
 	} else if (playerChoice == 'scissors' && computerChoice == 'paper') {
+		playerWins = playerWins + 1;
 		return('Computer chose Paper! You Win! Scissors beats Paper!');
 	} else 
 		return('Please enter either "Rock", "Paper", or" Scissors"');
+
+}
+//  Plays rock paper scissors for 5 rounds and displays the winner
+function game() {
+	while (playerWins < 5 && computerWins < 5) {
+		console.log(playRound()); 
+		console.log('Rounds won: ' + playerWins + ' Computer rounds won: ' + computerWins);
+	}
+	if (playerWins = 5) {
+		return('You won the game!')
+	} else {
+		return('You lost the game.')
+	}
 }
 
-
-
-// Take the player's selection and decide the winner
-	let playerChoice = prompt('Choose rock, paper, or scissors.');
-	playerChoice = playerChoice.toLowerCase();
-	const computerChoice = getComputerChoice();
-	console.log(playRound(playerChoice, computerChoice));
+// main
+let playerWins = 0;
+let computerWins = 0;
+console.log(game());
 
