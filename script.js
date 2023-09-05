010c277b4ed070adbf56a1c954b3604bf2deeab6
jongithub59 <jdelrealj01@gmail.com>
2023-09-05 16:50:22 -0500

    added new function tp play a round of rock paper made new function that compares the user and computer selections and decides the winner through a else if statement.

--- script.js
+++ script.js
@@ -4,16 +4,49 @@ function getComputerChoice() { // randomly generates a numeber 1-3 and uses a sw
 	let computerChoice = Math.floor(Math.random() * 3) + 1;
 	switch(computerChoice) {
 		case 1:
-			computerChoice = "Rock";
+			computerChoice = "rock";
 			break;
 		case 2:
-			computerChoice = "Paper";
+			computerChoice = "paper";
 			break;
 		case 3:
-			computerChoice = "Scissors";
+			computerChoice = "scissors";
 			break;
 	}
-	return computerChoice
+	return computerChoice;
 	
 }
-// Take the player's selection decide the winner
+
+function playRound(playerChoice, computerChoice) {
+	if (playerChoice == computerChoice) {
+		return('Tie!');
+
+	} else if(playerChoice == 'rock' && computerChoice == 'paper') {
+		return('Computer chose Paper! You Lose! Paper beats Rock!');
+
+	} else if (playerChoice == 'rock' && computerChoice == 'scissors') {
+		return('Computer chose Scissors! You Win! Rock beats Scissors!');
+
+	} else if (playerChoice == 'paper' && computerChoice == 'rock') {
+		return('Computer chose Rock! You Win! Paper beats Rock!');
+
+	} else if (playerChoice == 'paper' && computerChoice == 'scissors') {
+		return('Computer chose Scissors! You Lose! Scissors beats Paper!');
+
+	} else if (playerChoice == 'scissors' && computerChoice == 'rock') {
+		return('Computer chose Rock! You Lose! Rock beats Scissors!');
+
+	} else if (playerChoice == 'scissors' && computerChoice == 'paper') {
+		return('Computer chose Paper! You Win! Scissors beats Paper!');
+	} else 
+		return('Please enter either "Rock", "Paper", or" Scissors"');
+}
+
+
+
+// Take the player's selection and decide the winner
+	let playerChoice = prompt('Choose rock, paper, or scissors.');
+	playerChoice = playerChoice.toLowerCase();
+	const computerChoice = getComputerChoice();
+	console.log(playRound(playerChoice, computerChoice));
+
